a66787cbcd6fe884151a48cbeff12365f25ee0a9
abdurrahmankaya71 <abdurrahmanky71@gmail.com>
2024-02-16 15:32:00 +0300

    Upload Files

---
+++ js/main.js
@@ -0,0 +1,192 @@
+/*
+  Steps To Create The Project
+  [01] Create HTML Markup 
+  [02] Add Styling And Separate From Logic
+  [03] Create The App Logic
+  ---- [01] Add Levels
+  ---- [02] Show Level And Seconds
+  ---- [03] Add Array Of Words
+  ---- [04] ِAdd Start Game Button
+  ---- [05] Generate Upcoming Words
+  ---- [06] Disable Copy Word And Paste Event + Focus On Input
+  ---- [07] Start Play Function
+  ---- [08] Start The Time And Count Score
+  ---- [09] Add The Error And Success Messages
+  [04] Additonal Feature 
+  ---- [01] Save Score To Local Storage With Date
+  ---- [02] Choose Levels From Select Box
+  ---- [03] Break The Logic To More Functions
+  ---- [04] Choose Array Of Words For Every Level
+  ---- [05] Write Game Instruction With Dynamic Values
+  ---- [06] Add 3 Seconds For The First Word
+*/
+
+// Array Of Words
+const words = [
+    "Hello",
+    "Code",
+    "Town",
+    "Country",
+    "Testing",
+    "Youtube",
+    "Runner",
+    "Roles",
+    "Test",
+    "Rust",
+    "Playing",
+    "Funny",
+    "Linkedin",
+    "Twitter",
+    "Github",
+    "Leetcode",
+    "Internet",
+    "Python",
+    "Scala",
+    "Paradigm",
+    // "Styling",
+    // "Cascade",
+    // "Destructuring",
+    // "Coding",
+    // "Working",
+    // "Dependencies",
+    // "Task",
+    // "Documentation",
+    // "Programming",
+    // "Javascript",
+];
+
+// Catch Selectors
+let inputElements = document.getElementsByClassName("messageCheckbox");
+let difLvl = document.querySelector(".dif-lvl");
+let lvlSpan = document.querySelector(".lvl");
+let secondsSpan = document.querySelector(".message .seconds");
+let startBtn = document.querySelector(".start");
+let theWord = document.querySelector(".the-word");
+let input = document.querySelector(".input");
+let upcomingWords = document.querySelector(".upcoming-words");
+let timeLeftSpan = document.querySelector(".time span");
+let scoreGot = document.querySelector(".score .got");
+let scoreTotal = document.querySelector(".score .total");
+let finishMessage = document.querySelector(".finish");
+let repeatBtn = document.querySelector(".repeat");
+
+// Setting Levels
+const lvls = {
+    Easy: 5,
+    Medium: 4,
+    Hard: 3,
+};
+// Difficalty Default Level
+let defaultLvName = "Medium";
+let defaultLvSecound = lvls[defaultLvName];
+lvlSpan.innerHTML = defaultLvName;
+secondsSpan.innerHTML = defaultLvSecound;
+
+// Disaple Paste Event
+input.onpaste = () => {
+    return false;
+};
+
+document.getElementsByName("dif-lvl").forEach(function (chk) {
+    chk.addEventListener("click", function () {
+        if (this.checked) {
+            // Difficalty Level
+            defaultLvName = this.value;
+            defaultLvSecound = lvls[defaultLvName];
+            lvlSpan.innerHTML = defaultLvName;
+            secondsSpan.innerHTML = defaultLvSecound;
+            timeLeftSpan.innerHTML = defaultLvSecound;
+        }
+    });
+});
+
+// Total Score
+scoreTotal.innerHTML = words.length;
+
+// Start Play Function
+startBtn.onclick = function () {
+    this.remove();
+    difLvl.remove();
+    input.focus();
+
+    // Generate Word func.
+    genWord();
+};
+
+// Repeatbtn on click
+repeatBtn.onclick = function () {
+    location.reload();
+};
+
+function genWord() {
+    // Generate Word From the array
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    // Get the index of the Word
+    let wordIndex = words.indexOf(randomWord);
+    // Remove the word from array using Splice
+    words.splice(wordIndex, 1);
+    // Show the RandomWord
+    theWord.innerHTML = randomWord;
+    // Empty the upcaming words
+    upcomingWords.innerHTML = "";
+    // Generate the upcoming Words
+    for (let i = 0; i < words.length; i++) {
+        let div = document.createElement("div");
+        let txt = document.createTextNode(words[i]);
+        // Appending the words to the box
+        div.appendChild(txt);
+        upcomingWords.appendChild(div);
+    }
+
+    // Call Start Play Function
+    startPlay();
+}
+
+// Start Play the Time and the Score
+
+// Start Play Function
+function startPlay() {
+    // Reset the timeLeft
+    timeLeftSpan.innerHTML = defaultLvSecound;
+    let start = setInterval(() => {
+        timeLeftSpan.innerHTML--;
+        if (timeLeftSpan.innerHTML === "0") {
+            // Stop the timer
+            clearInterval(start);
+            // Change the words to lowerCase
+            if (
+                theWord.innerHTML.toLowerCase() ===
+                input.value.toLowerCase().trim()
+            ) {
+                // Increase Score
+                scoreGot.innerHTML++;
+                // Empty Input Field
+                input.value = "";
+                // Cheaking if the array didn't Finish
+                if (words.length > 0) {
+                    //  CallBack wordGen func.
+                    genWord();
+                } else {
+                    // On Winnig Situation
+                    upcomingWords.remove();
+                    // printing Congratulation
+                    let span = document.createElement("span");
+                    let winningMsg = document.createTextNode(
+                        "CONGRATULATION, YOU WON"
+                    );
+                    span.className = "good";
+                    span.appendChild(winningMsg);
+                    finishMessage.appendChild(span);
+                }
+            } else {
+                // If the user input wrong Input
+                let span = document.createElement("span");
+                span.className = "bad";
+                let gameOver = document.createTextNode("Game Over");
+                span.appendChild(gameOver);
+                // append the bad span to the finshMessege
+                finishMessage.appendChild(span);
+            }
+        }
+    }, 1000);
+}
